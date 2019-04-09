38f14125ea8764f1fa779be92f7828ba45705f9d
g.cheooool <g.cheooool@gmail.com>
2019-04-09 19:24:28 +0900

    mock data 파일 추가

---
+++ src/mock/data.js
@@ -0,0 +1,67 @@
+const data = [
+  {
+    id: 1,
+    parts: 'Parts',
+    name: 'Workout',
+    type: ['weight', 'reps'],
+    sets: [
+      {
+        weight: 50,
+        reps: 11
+      },
+      {
+        weight: 70,
+        reps: 11
+      },
+      {
+        weight: 90,
+        reps: 11
+      }
+    ],
+    createdAt: '2018-01-01'
+  },
+  {
+    id: 2,
+    parts: 'Parts2',
+    name: 'Workout2',
+    type: ['weight', 'reps'],
+    sets: [
+      {
+        weight: 30,
+        reps: 11
+      },
+      {
+        weight: 10,
+        reps: 11
+      },
+      {
+        weight: 50,
+        reps: 11
+      }
+    ],
+    createdAt: '2018-01-01'
+  },
+  {
+    id: 3,
+    parts: 'Parts3',
+    name: 'Workout3',
+    type: ['weight', 'reps'],
+    sets: [
+      {
+        weight: 50,
+        reps: 11
+      },
+      {
+        weight: 10,
+        reps: 11
+      },
+      {
+        weight: 50,
+        reps: 11
+      }
+    ],
+    createdAt: '2018-01-01'
+  }
+];
+
+export default data;
