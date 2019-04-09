1c976e721cbc821072ae744d72ad95098e0c2f8e
g.cheooool <g.cheooool@gmail.com>
2019-04-10 03:25:49 +0900

    운동 추가 기능
    - addWorkout 작업
    - 운동명이나 운동 타입 선택 없을 때 추가 안되도록 작업

--- src/components/Workout/WorkoutForm.tsx
+++ src/components/Workout/WorkoutForm.tsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import styled from 'styled-components';
+import { connect } from 'react-redux';
 import {
   Dialog,
   DialogTitle,
@@ -11,34 +11,39 @@ import {
   FormControlLabel,
   Switch
 } from '@material-ui/core';
+import { WorkoutType } from '../../types/workout';
+import { addWorkout } from '../../actions/workout';
 
 interface IWorkoutFormProps {
   open: boolean;
   onClose: () => void;
+  addWorkout: (workout: WorkoutType) => void;
 }
 
+const initialState = {
+  parts: '',
+  name: '',
+  type: [
+    {
+      label: 'Weight',
+      name: 'weight',
+      checked: true
+    },
+    {
+      label: 'Reps',
+      name: 'reps',
+      checked: true
+    },
+    {
+      label: 'Time',
+      name: 'time',
+      checked: false
+    }
+  ]
+};
+
 class WorkoutForm extends Component<IWorkoutFormProps> {
-  state = {
-    parts: '',
-    name: '',
-    type: [
-      {
-        label: 'Weight',
-        name: 'weight',
-        checked: true
-      },
-      {
-        label: 'Reps',
-        name: 'reps',
-        checked: true
-      },
-      {
-        label: 'Time',
-        name: 'time',
-        checked: false
-      }
-    ]
-  };
+  state = initialState;
 
   handleChangeText = (event: React.ChangeEvent<HTMLInputElement>) => {
     this.setState({
@@ -62,11 +67,40 @@ class WorkoutForm extends Component<IWorkoutFormProps> {
     });
   };
 
+  handleClose = () => {
+    this.props.onClose();
+  };
+
+  handleSubmit = () => {
+    const workout: WorkoutType = {
+      id: Math.random(),
+      ...this.state,
+      createdAt: new Date().getTime()
+    };
+
+    if (!this.isValid()) {
+      return false;
+    }
+
+    this.props.addWorkout(workout);
+    this.props.onClose();
+  };
+
+  isValid = () => {
+    const { name, type } = this.state;
+    const checkName = name !== '';
+    const checkType = type.some(value => value.checked === true);
+    if (!checkName || !checkType) {
+      return false;
+    }
+    return true;
+  };
+
   render() {
-    const { open, onClose } = this.props;
+    const { open } = this.props;
     const { parts, name, type } = this.state;
     return (
-      <Dialog open={open} keepMounted onClose={onClose}>
+      <Dialog open={open} keepMounted onClose={this.handleClose}>
         <DialogTitle>운동 추가</DialogTitle>
         <DialogContent>
           <TextField
@@ -89,11 +123,6 @@ class WorkoutForm extends Component<IWorkoutFormProps> {
                 <FormControlLabel
                   key={index}
                   control={
-                    // <Checkbox
-                    //   name={value.name}
-                    //   checked={value.checked}
-                    //   onChange={this.handleCheckbox}
-                    // />
                     <Switch
                       name={value.name}
                       checked={value.checked}
@@ -114,10 +143,15 @@ class WorkoutForm extends Component<IWorkoutFormProps> {
           </FormGroup>
         </DialogContent>
         <DialogActions>
-          <Button color="secondary" fullWidth={true} onClick={onClose}>
+          <Button color="secondary" fullWidth={true} onClick={this.handleClose}>
             취소
           </Button>
-          <Button variant="contained" color="primary" fullWidth={true}>
+          <Button
+            variant="contained"
+            color="primary"
+            fullWidth={true}
+            onClick={this.handleSubmit}
+          >
             추가
           </Button>
         </DialogActions>
@@ -126,4 +160,13 @@ class WorkoutForm extends Component<IWorkoutFormProps> {
   }
 }
 
-export default WorkoutForm;
+const mapDispatchToProps = (dispatch: any) => {
+  return {
+    addWorkout: (workout: WorkoutType) => dispatch(addWorkout(workout))
+  };
+};
+
+export default connect(
+  null,
+  mapDispatchToProps
+)(WorkoutForm);
