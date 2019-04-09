a112b181f4dcb558846714235821eec105c418bb
g.cheooool <g.cheooool@gmail.com>
2019-04-10 02:22:00 +0900

    modal -> dialog 수정

--- src/components/Workout/WorkoutForm.tsx
+++ src/components/Workout/WorkoutForm.tsx
@@ -1,55 +1,127 @@
 import React, { Component } from 'react';
 import styled from 'styled-components';
 import {
-  Modal,
-  Typography,
+  Dialog,
+  DialogTitle,
+  DialogContent,
+  DialogActions,
+  FormGroup,
   TextField,
   Button,
   FormControlLabel,
-  Checkbox
+  Switch
 } from '@material-ui/core';
 
-const WorkoutFormBox = styled.div`
-  position: absolute;
-  top: 50%;
-  left: 50%;
-  transform: translate(-50%, -50%);
-  width: 100%;
-  max-width: 480px;
-  padding: 1em;
-  box-sizing: border-box;
-  background: #fff;
-  box-shadow: 0 10px 20px rgba(0, 0, 0, 0.19), 0 6px 6px rgba(0, 0, 0, 0.23);
-`;
+interface IWorkoutFormProps {
+  open: boolean;
+  onClose: () => void;
+}
 
-const ButtonGroup = styled.div`
-  width: 100%;
-  display: flex;
-`;
+class WorkoutForm extends Component<IWorkoutFormProps> {
+  state = {
+    parts: '',
+    name: '',
+    type: [
+      {
+        label: 'Weight',
+        name: 'weight',
+        checked: true
+      },
+      {
+        label: 'Reps',
+        name: 'reps',
+        checked: true
+      },
+      {
+        label: 'Time',
+        name: 'time',
+        checked: false
+      }
+    ]
+  };
 
-class WorkoutForm extends Component {
+  handleChangeText = (event: React.ChangeEvent<HTMLInputElement>) => {
+    this.setState({
+      [event.target.name]: event.target.value
+    });
+  };
+
+  handleCheckbox = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const { type } = this.state;
+    const newType = type.map(value => {
+      if (value.name === event.target.name) {
+        return {
+          ...value,
+          checked: event.target.checked
+        };
+      }
+      return value;
+    });
+    this.setState({
+      type: [...newType]
+    });
+  };
+
   render() {
+    const { open, onClose } = this.props;
+    const { parts, name, type } = this.state;
     return (
-      <Modal open={false}>
-        <WorkoutFormBox>
-          <Typography variant="title">운동 추가</Typography>
-          <TextField label="부위" fullWidth={true} />
-          <TextField label="운동" fullWidth={true} />
-          <FormControlLabel
-            control={<Checkbox value="weight" />}
-            label="weight"
+      <Dialog open={open} keepMounted onClose={onClose}>
+        <DialogTitle>운동 추가</DialogTitle>
+        <DialogContent>
+          <TextField
+            label="부위"
+            fullWidth={true}
+            name="parts"
+            value={parts}
+            onChange={this.handleChangeText}
           />
-          <FormControlLabel control={<Checkbox value="reps" />} label="reps" />
-          <ButtonGroup>
-            <Button color="secondary" fullWidth={true}>
-              취소
-            </Button>
-            <Button variant="contained" color="primary" fullWidth={true}>
-              추가
-            </Button>
-          </ButtonGroup>
-        </WorkoutFormBox>
-      </Modal>
+          <TextField
+            label="운동"
+            fullWidth={true}
+            name="name"
+            value={name}
+            onChange={this.handleChangeText}
+          />
+          <FormGroup row={false}>
+            {type.map((value, index) => {
+              return (
+                <FormControlLabel
+                  key={index}
+                  control={
+                    // <Checkbox
+                    //   name={value.name}
+                    //   checked={value.checked}
+                    //   onChange={this.handleCheckbox}
+                    // />
+                    <Switch
+                      name={value.name}
+                      checked={value.checked}
+                      onChange={this.handleCheckbox}
+                      color="primary"
+                    />
+                  }
+                  label={value.label}
+                  value="end"
+                  labelPlacement="start"
+                  style={{
+                    marginLeft: '0px',
+                    justifyContent: 'space-between'
+                  }}
+                />
+              );
+            })}
+          </FormGroup>
+        </DialogContent>
+        <DialogActions>
+          <Button color="secondary" fullWidth={true} onClick={onClose}>
+            취소
+          </Button>
+          <Button variant="contained" color="primary" fullWidth={true}>
+            추가
+          </Button>
+        </DialogActions>
+      </Dialog>
     );
   }
 }
