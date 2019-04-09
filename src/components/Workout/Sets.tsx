7c8253cfc15ea7aa5e47ff999ce9a9c4e2951b95
g.cheooool <g.cheooool@gmail.com>
2019-04-10 05:48:48 +0900

    material-ui Paper 추가 및 데이터 없을 경우 null

--- src/components/Workout/Sets.tsx
+++ src/components/Workout/Sets.tsx
@@ -1,5 +1,6 @@
 import React, { FunctionComponent } from 'react';
 import {
+  Paper,
   Table,
   TableBody,
   TableCell,
@@ -14,41 +15,46 @@ interface ISetsProps {
 }
 
 const Sets: FunctionComponent<ISetsProps> = ({ type, data }) => {
+  if (!data.length) {
+    return null;
+  }
   return (
-    <Table>
-      <TableHead>
-        <TableRow>
-          <TableCell align="center">Set</TableCell>
-          <TableCell align="center">Data</TableCell>
-        </TableRow>
-      </TableHead>
-      <TableBody>
-        {data.map((row, index) => {
-          return (
-            <TableRow key={index}>
-              <TableCell component="td" scope="row" align="center">
-                {index + 1}
-              </TableCell>
-              <TableCell component="td" scope="row" align="center">
-                {type.map((value, typeIndex) => {
-                  return (
-                    value.checked && (
-                      <div
-                        key={`${index}-${typeIndex}`}
-                        style={{ margin: '1em 0' }}
-                      >
-                        <strong>{value.label.toUpperCase()}</strong>{' '}
-                        <span>{row[value.name]}</span>
-                      </div>
-                    )
-                  );
-                })}
-              </TableCell>
-            </TableRow>
-          );
-        })}
-      </TableBody>
-    </Table>
+    <Paper>
+      <Table>
+        <TableHead>
+          <TableRow>
+            <TableCell align="center">Set</TableCell>
+            <TableCell align="center">Data</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {data.map((row, index) => {
+            return (
+              <TableRow key={index}>
+                <TableCell component="td" scope="row" align="center">
+                  {index + 1}
+                </TableCell>
+                <TableCell component="td" scope="row" align="center">
+                  {type.map((value, typeIndex) => {
+                    return (
+                      value.checked && (
+                        <div
+                          key={`${index}-${typeIndex}`}
+                          style={{ margin: '1em 0' }}
+                        >
+                          <strong>{value.label.toUpperCase()}</strong>{' '}
+                          <span>{row[value.name]}</span>
+                        </div>
+                      )
+                    );
+                  })}
+                </TableCell>
+              </TableRow>
+            );
+          })}
+        </TableBody>
+      </Table>
+    </Paper>
   );
 };
 
