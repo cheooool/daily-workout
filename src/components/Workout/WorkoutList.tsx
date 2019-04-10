79727c55c164fccd421d448f90dac4deddb2eaf4
g.cheooool <g.cheooool@gmail.com>
2019-04-10 13:45:54 +0900

    List 스타일컴포넌트 추가

--- src/components/Workout/WorkoutList.tsx
+++ src/components/Workout/WorkoutList.tsx
@@ -2,6 +2,7 @@ import React, { FunctionComponent } from 'react';
 import { WorkoutListType, WorkoutType } from '../../types/workout';
 import { Typography } from '@material-ui/core';
 import Workout from './Workout';
+import styled from 'styled-components';
 
 interface IWorkoutListProps {
   list: WorkoutListType;
@@ -16,18 +17,33 @@ const WorkoutList: FunctionComponent<IWorkoutListProps> = ({
     return <Typography>등록된 운동이 없습니다.</Typography>;
   }
   return (
-    <div>
+    <List>
       {list.map((workout, index) => {
         return (
-          <Workout
-            key={index}
-            data={workout}
-            handleOpenUpdateForm={handleOpenUpdateForm}
-          />
+          <ListItem key={index}>
+            <Workout
+              data={workout}
+              handleOpenUpdateForm={handleOpenUpdateForm}
+            />
+          </ListItem>
         );
       })}
-    </div>
+    </List>
   );
 };
+
+const List = styled.ul`
+  list-style: none;
+  margin: 0;
+  padding: 0;
+`;
+
+const ListItem = styled.li`
+  margin: 2em 0;
+
+  &:first-child {
+    margin-top: 0;
+  }
+`;
 
 export default WorkoutList;
