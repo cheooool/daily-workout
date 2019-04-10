2fb04a4f874cf9ae83a2d31f7ddd58ba2bd08503
g.cheooool <g.cheooool@gmail.com>
2019-04-10 13:44:57 +0900

    table 수정
    - action th 추가
    - 세트 수정, 삭제 아이콘 추가
    - 세트 추가행 삽입

--- src/components/Workout/Sets.tsx
+++ src/components/Workout/Sets.tsx
@@ -5,9 +5,12 @@ import {
   TableBody,
   TableCell,
   TableHead,
-  TableRow
+  TableRow,
+  IconButton,
+  Button
 } from '@material-ui/core';
 import { SetsType, WorkoutCheckboxListType } from '../../types/workout';
+import { Add, Close, Edit } from '@material-ui/icons';
 
 interface ISetsProps {
   type: WorkoutCheckboxListType;
@@ -15,39 +18,71 @@ interface ISetsProps {
 }
 
 const Sets: FunctionComponent<ISetsProps> = ({ type, data }) => {
-  if (!data.length) {
-    return null;
-  }
   return (
     <Paper>
-      <Table>
+      <Table style={{ tableLayout: 'fixed' }}>
         <TableHead>
           <TableRow>
-            <TableCell align="center">Set</TableCell>
-            <TableCell align="center">Data</TableCell>
+            <TableCell style={tableCellStyles} align="center">
+              Set
+            </TableCell>
+            <TableCell style={tableCellStyles} align="center">
+              Data
+            </TableCell>
+            <TableCell style={tableCellStyles} align="center">
+              Actions
+            </TableCell>
           </TableRow>
         </TableHead>
         <TableBody>
-          {data.map((row, index) => {
+          <TableRow>
+            <TableCell style={tableCellStyles} align="center">
+              {data.length + 1}
+            </TableCell>
+            <TableCell style={tableCellStyles} align="center" colSpan={2}>
+              <Button fullWidth={true}>
+                <Add fontSize="small" color="primary" />
+              </Button>
+            </TableCell>
+          </TableRow>
+          {data.reverse().map((row, index) => {
             return (
               <TableRow key={index}>
-                <TableCell component="td" scope="row" align="center">
-                  {index + 1}
+                <TableCell
+                  style={tableCellStyles}
+                  component="td"
+                  scope="row"
+                  align="center"
+                >
+                  {data.length - index}
                 </TableCell>
-                <TableCell component="td" scope="row" align="center">
+                <TableCell
+                  style={tableCellStyles}
+                  component="td"
+                  scope="row"
+                  align="center"
+                >
                   {type.map((value, typeIndex) => {
                     return (
                       value.checked && (
                         <div
                           key={`${index}-${typeIndex}`}
-                          style={{ margin: '1em 0' }}
+                          style={{ margin: '1em 0', lineHeight: '1.5em' }}
                         >
-                          <strong>{value.label.toUpperCase()}</strong>{' '}
+                          <strong>{value.label.toUpperCase()}</strong> <br />
                           <span>{row[value.name]}</span>
                         </div>
                       )
                     );
                   })}
+                </TableCell>
+                <TableCell style={tableCellStyles}>
+                  <IconButton>
+                    <Edit fontSize="small" color="primary" />
+                  </IconButton>
+                  <IconButton>
+                    <Close fontSize="small" color="secondary" />
+                  </IconButton>
                 </TableCell>
               </TableRow>
             );
@@ -56,6 +91,11 @@ const Sets: FunctionComponent<ISetsProps> = ({ type, data }) => {
       </Table>
     </Paper>
   );
+};
+
+// 임시 스타일 추후 @material-ui/styles 추가하여 스타일 작업 필요
+const tableCellStyles = {
+  padding: '0'
 };
 
 export default Sets;
